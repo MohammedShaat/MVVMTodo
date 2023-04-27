bd7e97cb9ef3a7cacfaba08bf043d79d63dda658
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-27 14:23:46 +0200

    Set up TaskListAdapter to display tasks

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksAdapter.kt
@@ -0,0 +1,54 @@
+package com.codinginflow.mvvmtodo.ui.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.codinginflow.mvvmtodo.data.Task
+import com.codinginflow.mvvmtodo.databinding.ItemTaskBinding
+
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TaskViewHolder>(TaskCallBack()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class TaskViewHolder private constructor(private val binding: ItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun create(parent: ViewGroup): TaskViewHolder {
+                val binding = ItemTaskBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                return TaskViewHolder(binding)
+            }
+        }
+
+        fun bind(task: Task) {
+            binding.apply {
+                checkBoxCompleteTask.isChecked = task.completed
+                textViewTaskName.text = task.name
+                textViewTaskName.paint.isStrikeThruText = task.completed
+                imageViewPriority.isVisible = task.important
+            }
+        }
+    }
+
+    class TaskCallBack : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
