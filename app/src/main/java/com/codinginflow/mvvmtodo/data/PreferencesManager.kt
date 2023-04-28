6397dd39662e229a0e8728d07e5f080518177fc5
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-28 11:16:58 +0200

    Fix @ApplicationContext error

--- app/src/main/java/com/codinginflow/mvvmtodo/data/PreferencesManager.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/PreferencesManager.kt
@@ -1,17 +1,24 @@
 package com.codinginflow.mvvmtodo.data
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.preferences.createDataStore
 import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.emptyPreferences
 import androidx.datastore.preferences.preferencesKey
 import com.codinginflow.mvvmtodo.di.ApplicationCoroutine
 import com.codinginflow.mvvmtodo.ui.tasks.SortOrder
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
+private const val TAG = "PreferencesManager"
+
 @Singleton
-class PreferencesManager @Inject constructor(@ApplicationCoroutine context: Context) {
+class PreferencesManager @Inject constructor(@ApplicationContext context: Context) {
 
     private object PreferencesKeys {
         val sortOrder = preferencesKey<String>("sort_order")
@@ -20,7 +27,17 @@ class PreferencesManager @Inject constructor(@ApplicationCoroutine context: Cont
 
     private val dataStore = context.createDataStore("user_preferences")
 
-    val preferencesFilterFlow = dataStore.data.map { preferences ->
+    val preferencesFilterFlow = dataStore.data
+        .catch { e ->
+            if (e is IOException) {
+                Log.e(TAG, "Error reading preferences", e )
+                emit(emptyPreferences())
+            } else
+            {
+                throw e
+            }
+        }
+        .map { preferences ->
         val sortOrder = SortOrder.valueOf(
             preferences[PreferencesKeys.sortOrder] ?: SortOrder.BY_DATE_CREATED.name
         )
