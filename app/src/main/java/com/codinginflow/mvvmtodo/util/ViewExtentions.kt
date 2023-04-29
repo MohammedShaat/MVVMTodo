6abba15d96ac8924736ebb201492c40c9e09583b
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-04-29 20:39:58 +0300

    Restore SearchView's query

--- app/src/main/java/com/codinginflow/mvvmtodo/util/ViewExtentions.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/util/ViewExtentions.kt
@@ -2,14 +2,14 @@ package com.codinginflow.mvvmtodo.util
 
 import androidx.appcompat.widget.SearchView
 
-fun SearchView.onQueryTextChange(block: (String) -> Unit) {
+fun SearchView.onQueryTextChange(block: ((String) -> Unit)?) {
     setOnQueryTextListener(object : SearchView.OnQueryTextListener {
         override fun onQueryTextSubmit(query: String?): Boolean {
             return true
         }
 
         override fun onQueryTextChange(newText: String?): Boolean {
-            block(newText.orEmpty())
+            block?.invoke(newText.orEmpty())
             return true
         }
     })
